29b287a49f308219b7bbb7872e1f2ba48890a64e
Courtnee <davis.courtnee22@gmail.com>
2019-10-20 00:43:20 -0400

    Added Home and FAQ components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
+import Faq from './components/Faq/Faq';
+import { Route } from 'react-router-dom';
 import './App.css';
 import './App.scss';
 
@@ -8,6 +11,9 @@ function App() {
   return (
     <div className="App">
       <Header></Header> 
+
+        <Route exact={true} path="/" component={Home} />
+        <Route path="/Faq" component={Faq} />
 
       <Footer></Footer>
     </div>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <div className="Header">
-        My Header
+<div className="navbar" role="navigation" aria-label="main navigation">  
+
+  <div id="navbarBasicExample" className="navbar-menu">
+  
+    <div className="navbar-start">
+        <Link to="/" className="navbar-item">Home</Link>
+        <Link to="/about" className="navbar-item">About</Link>
+        <Link to="/features" className="navbar-item">Features</Link>
+        <Link to="/faq" className="navbar-item">FAQ</Link>
     </div>
+
+    <div className="navbar-end">
+      <div className="navbar-item">
+        <div className="buttons">
+          <a className="button is-primary">
+            <strong>Join Now</strong>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
   );
 }
 
