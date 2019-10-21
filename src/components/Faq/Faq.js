1cb58c9d9c35ac4c28a8dc9edef1bc56e914f4db
Courtnee <davis.courtnee22@gmail.com>
2019-10-20 21:14:40 -0400

    Created FAQ Page

--- src/components/Faq/Faq.js
+++ src/components/Faq/Faq.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
+import "./Faq.scss"
+import Axios from 'axios';
 
 export default class Faq extends Component {
+   
   render() {
     return (
       <div>
-        FAQ
+        <div className="container">
+        <section className="section">
+            <h1 className="title">FAQ</h1>
+            <h2 className="subtitle is-4">Subheader</h2>
+        </section>
+        </div>
       </div>
     )
   }
